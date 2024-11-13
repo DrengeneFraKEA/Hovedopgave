1dda0f20d73fa3f504723fd20071e8fcecb33e95
KristianHjelmsmarkKEA <kristianhjelmsmark@gmail.com>
2024-11-13 13:11:19 +0100

    Added unit testing project to solution

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using Hovedopgave.Server;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+
+        [Fact]
+        public void TemperatureF_CalculatesCorrectly()
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast { TemperatureC = 0 };
+
+            // Act
+            int temperatureF = weatherForecast.TemperatureF;
+
+            // Assert
+            Assert.Equal(32, temperatureF);
+        }
+    }
+}
