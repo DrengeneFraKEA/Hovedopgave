eee291dd8c755057037368fe198b013fe4f173fe
Abdi Abdulle <abdi.abdulle@outlook.dk>
2024-11-18 10:36:40 +0100

    adminrights html & css modified and login component folder removed

--- hovedopgave.client/src/app/app-routing.module.ts
+++ hovedopgave.client/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminrightsComponent } from './components/adminrights/adminrights.component';
-import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'adminrights', component: AdminrightsComponent },
-  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- hovedopgave.client/src/app/app.module.ts
+++ hovedopgave.client/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminrightsComponent } from './components/adminrights/adminrights.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminrightsComponent,
-    LoginComponent
+    AdminrightsComponent
   ],
   imports: [
     BrowserModule,
