c9153a6e8b1a38a14bcd338884fff8b3267716b6
Abdi Abdulle <abdi.abdulle@outlook.dk>
2024-11-19 10:19:10 +0100

    adminrights page implemented, but hardcoded for now

--- hovedopgave.client/src/app/components/adminrights/adminrights.component.ts
+++ hovedopgave.client/src/app/components/adminrights/adminrights.component.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AdminrightsComponent {
   title = 'Admin Rights';
+  isModalOpen: boolean = false;
+  selectedUser: { username: string } | null = null;
+
+
+  openEditModal(user: { username: string }) {
+    this.selectedUser = user;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedUser = null;
+  }
+
+
+  sendNewPassword() {
+    // Logic
+  }
+
+  deleteUser() {
+    // Logic
+  }
+
+  submitChanges() {
+    // Logic
+  }
+
 
 }
