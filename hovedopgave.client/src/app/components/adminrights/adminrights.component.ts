d245de3e432761d5a6cbe4bc6cec5e3b38e520e5
Alpay <alpay_yilmaz@hotmail.com>
2024-11-22 14:18:18 +0100

    with redirect

--- hovedopgave.client/src/app/components/adminrights/adminrights.component.ts
+++ hovedopgave.client/src/app/components/adminrights/adminrights.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminrights',
   templateUrl: './adminrights.component.html',
   styleUrl: './adminrights.component.css'
 })
-export class AdminrightsComponent {
+export class AdminrightsComponent implements OnInit {
   title = 'Admin Rights';
   isModalOpen: boolean = false;
   selectedUser: { username: string } | null = null;
   passwordResetMessage: string | null = null;
+
+  constructor(private http: HttpClient, private route: Router) { }
+
+  ngOnInit() {
+    var token = localStorage.getItem("token");
+    if (token === null || token === "") this.route.navigate(['login']);
+  }
 
 
   openEditModal(user: { username: string }) {
